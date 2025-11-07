1a91e0d716f5803b1046e1ea733fd12aed9280de
yujihoboopa <yujiho0102@gmail.com>
2025-11-07 20:41:59 +0900

    start

--- flaskr/crawler/__init__.py
+++ flaskr/crawler/__init__.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+import time
 import os
 import json
 
@@ -9,43 +10,42 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-driver = webdriver.Firefox()
-
-driver.get("https://kyungheeboy.riroschool.kr")
+default_url = os.environ.get("DEFAULT_URL")
 
 
-def login():
-    driver.get("https://kyungheeboy.riroschool.kr/user.php?action=signin")
-    login_id = os.environ.get("USER_ID")
-    login_pw = os.environ.get("USER_PW")
-    driver.find_element(By.ID, "id").send_keys(login_id)
-    driver.find_element(By.ID, "pw").send_keys(login_pw)
+def main():
 
-    login_button = driver.find_element(
-        By.CSS_SELECTOR, "button.button_normal[type='button']"
-    )
-    login_button.click()
+    def login():
+        driver = webdriver.Firefox()
+
+        driver.get(default_url)
+
+        driver.get("https://kyungheeboy.riroschool.kr/user.php?action=signin")
+        login_id = os.environ.get("USER_ID")
+        login_pw = os.environ.get("USER_PW")
+        driver.find_element(By.ID, "id").send_keys(login_id)
+        driver.find_element(By.ID, "pw").send_keys(login_pw)
+
+        login_button = driver.find_element(
+            By.CSS_SELECTOR, "button.button_normal[type='button']"
+        )
+        login_button.click()
+
+        return driver
+
+    driver = login()
+    # driver.find_element(By.CLASS_NAME, "btn_sch_pc").click()
+    #
+    # time.sleep(10)
+    #
+    # html = driver.find_element(By.CLASS_NAME, "lds-wap")
+    # source = html.get_attribute("outerHTML")
+    #
+    # with open("bibi.html", "w") as file:
+    #     file.write(source)
+    #
+    # driver.find_element(By.CLASS_NAME, "lds-wap")
 
 
-# def get_cookies():
-#     with open("/home/jiho/riroBackShoter/crawler/cookie.txt", "w") as sessionFile:
-#         newList = []
-#         for cookie in driver.get_cookies():
-#             newCookie = {"name": cookie["name"], "value": cookie["value"]}
-#             newList.append(newCookie)
-#
-#         sessionFile.write(json.dumps(newList))
-#
-#
-# def load_cookies():
-#     with open("/home/jiho/riroBackShoter/crawler/cookie.txt", "r") as sessionFile:
-#         newList = sessionFile.read()
-#         cookies = json.loads(newList)
-#
-#         for cookie in cookies:
-#             driver.delete_cookie(cookie["name"])
-#             driver.add_cookie(cookie)
-#
-
-login()
+if __name__ == "__main__":
+    main()
